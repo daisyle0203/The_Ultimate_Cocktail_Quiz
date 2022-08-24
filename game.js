586f22cb6bd33d670a7a30c48c74c0b21f839884
Daisy Le <daisy.le0203@gmail.com>
2022-08-24 14:49:22 -0500

    Added quiz questions

---
+++ game.js
@@ -0,0 +1,40 @@
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const time = document.querySelector("#time");
+
+
+
+
+
+
+
+
+
+
+
+
+// 1. What gives a whiskey sour its frothy top?
+//  Marshmallow
+//  Lemon juice
+//  Egg white X
+//  Cream
+// 2. Which destination shares its name with a cocktail containing whiskey and sweet vermouth?
+//  Moscow Mule
+//  Cuba Libre
+//  Manhattan X
+//  Panama
+// 3. What transforms a Classic Martini to a Dirty Martini?
+//  Tomato juice
+//  Lemon juice
+//  Lime juice
+//  Olive juice X
+//  4. If you mix Guinness and Champagne, what cocktail do you get?
+//  Black Jack
+//  Black Russian
+//  Black Velvet X
+//  Black Widow
+// 4.Which classic cocktail includes vodka, triple sec, cranberry juice, and lime juice?
+// Old Fashioned
+// Mojito
+// Margarita
+// Cosmopolitan X
